8c08d7fc4c6bf76d93ad8617e3539e1c109fd5b4
Junior Johane <j.r.t.johane@gmail.com>
2022-07-21 12:35:37 +0200

    Third Commit: Changed Threads during a coRoutine

--- app/src/main/java/com/example/coroutinesdemo1/MainActivity.kt
+++ app/src/main/java/com/example/coroutinesdemo1/MainActivity.kt
@@ -5,13 +5,11 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
-    //Initialisations
+    //Initialisations and reference variables
+    private lateinit var messageText: TextView
     private var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +19,7 @@ class MainActivity : AppCompatActivity() {
         val textView = findViewById<TextView>(R.id.tvCount)
         val countButton = findViewById<Button>(R.id.btnCount)
         val downloadButton = findViewById<Button>(R.id.btnDownload)
+        messageText = findViewById(R.id.tvMessage)
 
         //setting click listener for the count button
         countButton.setOnClickListener {
@@ -41,6 +40,11 @@ class MainActivity : AppCompatActivity() {
     private suspend fun downloadUserData() {
         for (i in 1..200000) {
             Log.i("MyTag", "Downloading user $i in ${Thread.currentThread().name}")
+            //To change from UI thread to Main Thread
+            withContext(Dispatchers.Main){
+                messageText.text = "Downloading user $i"
+            }
+
             //To delay the output shown in Logcat, use suspend keyword in function, as well as code below
             delay(100)
         }
